937e2fdf85986ea407d4d36b8fcaeffde32d18c0
ChandlerMurphy <chandlermurphy02@gmail.com>
2025-05-29 19:12:04 -0600

    Added OAuth to the assignment. Now I am just testing and making my video for the rubric submission.

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const validation = require('../middleware/validator.js')
+const validation = require('../middleware/validator.js');
+const { isAuthenticated } = require('../middleware/authenticate.js');
 const router = express.Router();
 
 const contactsController = require('../controllers/contacts.js');
 
 router.get('/', contactsController.getAll);
 router.get('/:id', contactsController.getSingle);
-// router.post('/', contactsController.createContact);
-router.post('/', validation.saveContact, contactsController.createContact)
-// router.put('/:id', contactsController.updateContact);
-router.put('/:id', validation.saveContact, contactsController.updateContact);
-router.delete('/:id', contactsController.deleteContact);
+// router.post('/', validation.saveContact, contactsController.createContact)
+// router.put('/:id', validation.saveContact, contactsController.updateContact);
+// router.delete('/:id', contactsController.deleteContact);
+router.post('/', isAuthenticated, validation.saveContact, contactsController.createContact)
+router.put('/:id', isAuthenticated, validation.saveContact, contactsController.updateContact);
+router.delete('/:id', isAuthenticated, contactsController.deleteContact);
 
 module.exports = router;
--- routes/locations.js
+++ routes/locations.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const validation = require('../middleware/validator.js')
+const { isAuthenticated } = require('../middleware/authenticate.js');
 const router = express.Router();
 
 const locationsController = require('../controllers/locations.js');
 
 router.get('/', locationsController.getAllLocations);
 router.get('/:id', locationsController.getSingleLocation);
-// router.post('/', locationsController.createLocation);
-router.post('/', validation.saveLocation, locationsController.createLocation);
-// router.put('/:id', locationsController.updateLocation);
-router.put('/:id', validation.saveLocation, locationsController.updateLocation);
-router.delete('/:id', locationsController.deleteLocation);
+// router.post('/', validation.saveLocation, locationsController.createLocation);
+// router.put('/:id', validation.saveLocation, locationsController.updateLocation);
+// router.delete('/:id', locationsController.deleteLocation);
+router.post('/', isAuthenticated, validation.saveLocation, locationsController.createLocation);
+router.put('/:id', isAuthenticated, validation.saveLocation, locationsController.updateLocation);
+router.delete('/:id', isAuthenticated, locationsController.deleteLocation);
 
 module.exports = router;
